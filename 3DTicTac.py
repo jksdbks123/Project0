8237eb62faab58b3bedefa0efbe0d31ea9b65db1
zhihui <zhChen0412@outlook.com>
2022-10-05 13:32:03 -0700

    CS682

--- 3DTicTac.py
+++ 3DTicTac.py
@@ -37,7 +37,6 @@ class TicTacToe3D(object):
         self.map_seq_to_ind, self.map_ind_to_seq = self.create_map()
         self.allowed_moves = list(range(pow(3, 3)))
         self.difficulty = ply # steps looking ahead
-        self.depth_count = 0
         if player == player_1:
             self.player_1_turn = True
         else:
@@ -45,6 +44,7 @@ class TicTacToe3D(object):
         self.player_1 = player_1
         self.player_2 = player_2
         self.players = (self.player_1, self.player_2)
+
         # self.is_terminate = False
 
     def create_map(self):
@@ -78,68 +78,85 @@ class TicTacToe3D(object):
         self.np_board = self.create_board()
         self.depth_count = 0
 
-    def max(self):
+    def max(self,recursive_count,alpha, beta):
+        # print(recursive_count)
         maxv = -2
         px = None
         py = None
         pz = None
         result = is_terminate(self.np_board)
-        if result is None: #Draw
-            return (0,0,0,0)
-        elif result == 1:
-            return (1,0,0,0)
-        elif result == -1:
-            return (-1,0,0,0)
-        
-        player = self.player_2 #1 
-        if self.player_1_turn:
-            player = self.player_1 #-1
-        for i in range(self.np_board.shape[0]):
-            for j in range(self.np_board.shape[1]):
-                for k in range(self.np_board.shape[2]):
-                    if self.np_board[i,j,k] == 0:
-                        self.np_board[i,j,k] = player
-                        self.player_1_turn = ~self.player_1_turn
-                        m,px,py,pz = self.min()
-                        if m > maxv:
-                            maxv = m
-                            px,py,pz = i,j,k
-                        self.np_board[i,j,k] = 0
+        if recursive_count < self.difficulty:
 
+            if self.player_1_turn:
+                player = self.player_1
+            else:
+                player = self.player_2
+
+            if (result is None): # Tie
+                return (0,0,0,0)
+            elif result == player:
+                return (1,0,0,0)
+            elif result == -player:
+                return (-1,0,0,0)
+                
+            # if return is 0, return the maxv
+            for i in range(self.np_board.shape[0]):
+                for j in range(self.np_board.shape[1]):
+                    for k in range(self.np_board.shape[2]):
+                        if self.np_board[i,j,k] == 0:
+                            self.np_board[i,j,k] = player
+                            m,minx,miny,minz = self.min(recursive_count + 1, alpha, beta)
+                            if m > maxv:
+                                maxv = m
+                                px,py,pz = i,j,k
+                            self.np_board[i,j,k] = 0
+
+                            if maxv >= beta:
+                                return maxv,px,py,pz
+                            if maxv > alpha:
+                                alpha = maxv 
+        
         return maxv,px,py,pz
 
                         
         
-    def min(self):
+    def min(self,recursive_count,alpha, beta):
         minv = 2
-        px = None
-        py = None
-        pz = None
+        qx = None
+        qy = None
+        qz = None
         result = is_terminate(self.np_board)
-        if result is None: #Draw
-            return (0,0,0,0)
-        elif result == 1:
-            return (1,0,0,0)
-        elif result == -1:
-            return (-1,0,0,0)
-        
-        player = self.player_2 #1 
-        if self.player_1_turn:
-            player = self.player_1 #-1
+        if recursive_count < self.difficulty:
+            if self.player_1_turn:
+                player = self.player_1
+            else:
+                player = self.player_2
 
-        for i in range(self.np_board.shape[0]):
-            for j in range(self.np_board.shape[1]):
-                for k in range(self.np_board.shape[2]):
-                    if self.np_board[i,j,k] == 0:
-                        self.np_board[i,j,k] = player
-                        m,px,py,pz = self.min()
+            if result is None: #Tie
+                return (0,0,0,0)
+            elif result == player:
+                return (1,0,0,0)
+            elif result == -player:
+                return (-1,0,0,0)
+            
+            for i in range(self.np_board.shape[0]):
+                for j in range(self.np_board.shape[1]):
+                    for k in range(self.np_board.shape[2]):
+                        
+                        if self.np_board[i,j,k] == 0:
+                            self.np_board[i,j,k] = -player
+                            m,maxx,maxy,maxz = self.max(recursive_count + 1,alpha, beta)
+                            if m < minv:
+                                
+                                minv = m
+                                qx,qy,qz = i,j,k
+                            self.np_board[i,j,k] = 0
 
-                        if m < minv:
-                            minv = m
-                            px,py,pz = i,j,k
-                        self.np_board[i,j,k] = 0
-
-        return minv,px,py,pz
+                            if minv <= alpha:
+                                return minv,qx,qy,qz
+                            if minv < beta:
+                                alpha = minv 
+        return minv,qx,qy,qz
             
 
     @staticmethod
@@ -158,13 +175,39 @@ class TicTacToe3D(object):
         Until the game is complete we will alternate between computer and
         player turns while printing the current game state.
         """
+        Round = 0
         while True:
             
+            flag = is_terminate(self.np_board)
 
-            try:
+            if flag is None:
+                print("Draw!!")
+                break
+            if flag == self.player_1:
+                print("Player1 Win")
                 return self.np_board, self.player_1
-            except KeyboardInterrupt:
-                print('\n ctrl-c detected, exiting')
+                break 
+            if flag == self.player_2:
+                print("Player2 Win")
+                return self.np_board, self.player_2
+                break
+            if self.player_1_turn:
+                print('Round',Round,'Player1')
+                print(self.np_board)
+                Round += 1 
+                # player1's turn
+                (m, px, py, pz) = self.max(0,-2, 2)
+                self.np_board[px, py, pz] = self.player_1
+                self.player_1_turn = not self.player_1_turn
+                
+            else:
+                # player2's turn
+                print('Round',Round,'Player2')
+                print(self.np_board)
+                (m, px, py, pz) = self.max(0,-2,2)
+                self.np_board[px, py, pz] = self.player_2
+                self.player_1_turn = not self.player_1_turn
+
 
 
 
@@ -180,6 +223,6 @@ if __name__ == '__main__':
                 type=int, default=6
     )
     args = parser.parse_args()
-    brd,winner = TicTacToe3D(player=args.player, ply=args.ply).play_game()
+    brd,winner = TicTacToe3D(player=args.player, ply=6).play_game()
     print("final board: \n{}".format(brd))
     print("winner: player {}".format(winner))
--- Utils.py
+++ Utils.py
@@ -1,4 +1,5 @@
 from sre_parse import fix_flags
+from sys import flags
 import numpy as np 
 
 def if_line_win(test_line):
@@ -27,7 +28,7 @@ def if_plane_win(test_plane):
     if flag != 0:
         return flag
     # Diagonal Win #2
-    test_line = test_plane[np.arange(test_plane.shape[0]),np.arange(np_board.shape[0])[::-1]]
+    test_line = test_plane[np.arange(test_plane.shape[0]),np.arange(test_plane.shape[0])[::-1]]
     flag = if_line_win(test_line)
     if flag != 0:
         return flag
@@ -35,8 +36,7 @@ def if_plane_win(test_plane):
                
 def is_terminate(board_state):
     # Test three dimensional planes
-    if (board_state != 0).sum() == 27:
-        return None
+    
     for i in range(board_state.shape[0]):
         test_plane = board_state[i,:,:]
         flag = if_plane_win(test_plane)
@@ -69,6 +69,8 @@ def is_terminate(board_state):
     flag = if_line_win(test_line)
     if flag != 0:
         return flag
+    if (board_state != 0).sum() == 27:
+        return None # Tie
     return 0   #No one wins right now
     
         
@@ -76,14 +78,28 @@ def is_terminate(board_state):
                     
 
 if __name__ == '__main__':
-    # test_plane = np.zeros((3,3), dtype=int)
-    # test_plane[0,0] = -1
-    # test_plane[2,2] = -1
-    # test_plane[0,1] = 1
-    # test_plane[1,1] = 1
-    # test_plane[2,1] = 1
     np_board = np.zeros((3,3,3), dtype=int)
-    np_board[0,0,0] = -1
-    np_board[1,1,1] = -1
-    np_board[2,2,2] = -1
-    print(is_terminate(np_board))
+    Round = 0
+    while True:
+        flag = is_terminate(np_board)
+        if (flag is not None)&(flag != 0):
+            print('Done,Winner is:',flag)
+            print(np_board)
+            break
+        while True:
+            random_p = [np.random.choice([0,1,2]),np.random.choice([0,1,2]),np.random.choice([0,1,2])]
+            if np_board[random_p[0],random_p[1],random_p[2]] == 0:
+                break
+        np_board[random_p[0],random_p[1],random_p[2]] = 1
+        while True:
+            random_p = [np.random.choice([0,1,2]),np.random.choice([0,1,2]),np.random.choice([0,1,2])]
+            if np_board[random_p[0],random_p[1],random_p[2]] == 0:
+                break
+        np_board[random_p[0],random_p[1],random_p[2]] = -1
+        
+        
+        Round += 1
+        print('Round',Round)
+        print('----------')
+        print(np_board)
+
